aeeefe7ca60049224ae08f5b833a860bc227b0d3
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-07 21:20:41 -0500

    Proyecto Para Calcular Consumo Electrico Del Mes

---
+++ src/MainWindow/MainConsumoKW.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MainWindow;
+
+import igu.ConsumoKwGU;
+
+/**
+ *
+ * @author zDefcon
+ */
+public class MainConsumoKW {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    ConsumoKwGU Window = new ConsumoKwGU();
+    Window.setVisible(true);
+    Window.setResizable(false);
+    Window.setLocationRelativeTo(null);
+  }
+  
+}
